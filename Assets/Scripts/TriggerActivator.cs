e5051224fa52cbe0bb8461001387de35354b1a91
MayaWhat <ian@devolutious.co.nz>
2023-02-05 11:32:23 +1300

    Visuals etc

--- Assets/Scripts/TriggerActivator.cs
+++ Assets/Scripts/TriggerActivator.cs
@@ -3,12 +3,23 @@ using UnityEngine;
 
 public class TriggerActivator : Activator
 {
+    [SerializeField] private Sprite _activatedSprite;
+    [SerializeField] private Sprite _deactivatedSprite;
+
+    private SpriteRenderer _renderer;
+
     private List<Collider2D> _activators = new List<Collider2D>();
+
+    private void Awake()
+    {
+        _renderer = GetComponentInChildren<SpriteRenderer>();
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (_activators.Count == 0)
         {
+            _renderer.sprite = _activatedSprite;
             Activate();
         }
         _activators.Add(other);
@@ -20,6 +31,7 @@ public class TriggerActivator : Activator
 
         if (_activators.Count == 0)
         {
+            _renderer.sprite = _deactivatedSprite;
             Deactivate();
         }
     }
