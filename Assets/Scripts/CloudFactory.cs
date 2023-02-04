c672e5b4dbeb03522eb86d26bcdd42cf6628b09e
Ben Stewart <Ben-Stewart@live.com>
2023-02-05 10:38:47 +1300

    Add initial cloud burst

--- Assets/Scripts/CloudFactory.cs
+++ Assets/Scripts/CloudFactory.cs
@@ -5,11 +5,13 @@ public class CloudFactory : MonoBehaviour
 {
     [SerializeField] private List<Sprite> _cloudSprites;
     [SerializeField] private GameObject _cloudPrefab;
+    private float _levelWidth = 100f;
     private List<GameObject> _clouds = new List<GameObject>();
-    private int _cloudSpawnDeviation = 10;
-    private int _cloudSpawnFrequency = 40;
+    private int _cloudSpawnDeviation = 200;
+    private int _cloudSpawnFrequency = 1000;
     private int _cloudTimer = 0;
     private int _lastCloudSector = 0;
+    private int _initialClouds = 20;
     [SerializeField] private GameObject _camera;
     private float _parallaxEffect = 0.9f;
     private float _startPosition;
@@ -19,27 +21,42 @@ public class CloudFactory : MonoBehaviour
         _startPosition = transform.position.x;
     }
 
+    private void OnEnable()
+    {
+        var seperation = _levelWidth / _initialClouds;
+
+        for (int i = 0; i < _initialClouds; i++)
+        {
+            SpawnCloud(i * seperation);
+        }
+    }
+
     private void FixedUpdate()
     {
         if (_cloudTimer <= 0)
         {
-            var cloudY = Random.Range(4, 20);
-            if (cloudY / 10 == _lastCloudSector)
-            {
-                cloudY -= 5;
-            }
-            _lastCloudSector = cloudY / 10;
-            GameObject cloud = Instantiate(_cloudPrefab, new Vector3(_camera.transform.position.x + 20f, cloudY), new Quaternion());
-            cloud.GetComponent<SpriteRenderer>().sprite = _cloudSprites[Random.Range(0, _cloudSprites.Count)];
-            _clouds.Add(cloud);
-            cloud.transform.parent = transform;
-
+            SpawnCloud(_levelWidth);
             _cloudTimer = _cloudSpawnFrequency + Random.Range(-_cloudSpawnDeviation, _cloudSpawnDeviation);
         }
         else
         {
             _cloudTimer--;
         }
+    }
+
+    private void SpawnCloud(float xPosition)
+    {
+        var cloudY = Random.Range(6, 25);
+        if (cloudY / 10 == _lastCloudSector)
+        {
+            cloudY -= 5;
+        }
+        _lastCloudSector = cloudY / 10;
+
+        GameObject cloud = Instantiate(_cloudPrefab, new Vector3(xPosition, cloudY), new Quaternion());
+        cloud.GetComponent<SpriteRenderer>().sprite = _cloudSprites[Random.Range(0, _cloudSprites.Count)];
+        _clouds.Add(cloud);
+        cloud.transform.parent = transform;
     }
 
     private void Update()
